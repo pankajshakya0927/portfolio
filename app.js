c4ce1f923748fda9e9fe811a5e7ca6d203f08fa4
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-01-28 03:31:48 +0530

    Style changes: fade in animation, background gradient, header, laptop-mockup

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const salutationTextContainer = document.querySelector(".salutationTextContainer");
 const salutationImgContainer = document.querySelector(".salutationImgContainer");
-const nameContainer = document.querySelector(".nameContainer");
-const profilePicContainer = document.querySelector(".profilePicContainer");
 const jobTitleContainer = document.querySelector(".jobTitleContainer");
 const skillsContainer = document.querySelector(".skillsContainer");
 const projectsTitle = document.querySelector(".projectsTitle");
+const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
     let offsetY = window.scrollY;
     salutationTextContainer.style.transform = `translateY(${offsetY * 0.5}px)`;
     salutationImgContainer.style.transform = `translateY(${offsetY * 0.5}px)`;
-    nameContainer.style.transform = `translateX(${offsetY * 0.1}px)`;
-    profilePicContainer.style.transform = `translateX(${-offsetY * 0.1}px)`;
-    jobTitleContainer.style.transform = `translateY(calc(200vh - ${offsetY}px))`;
+    jobTitleContainer.style.transform = `translateY(calc(190vh - ${offsetY}px))`;
     skillsContainer.style.transform = `translateX(calc(250vh - ${offsetY}px))`;
-    projectsTitle.style.transform = `translateX(calc(-300vh + ${offsetY}px))`;
+    projectsTitle.style.transform = `translateX(calc(-320vh + ${offsetY}px))`;
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+      
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
 })
