03d95d8ad76b0d70c08ea09ccd5832c33d990cad
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-02-12 03:00:29 +0530

    Design changes

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function scrollToView(elementWithClass) {
-    document.querySelector(elementWithClass).scrollIntoView({behavior: 'smooth'});
+    document.querySelector(elementWithClass).scrollIntoView({ behavior: 'smooth' });
 }
+
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {};
+
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+});
