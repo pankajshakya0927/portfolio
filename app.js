ae6a4404d14c01c48f868ae7b19667f09a6e47f2
Pankaj Shakya <pankaj@metadesignsolutions.co.uk>
2024-03-10 22:48:41 +0545

    Adding a control center

--- app.js
+++ app.js
@@ -16,3 +16,57 @@ const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
 ELs_inViewport.forEach(EL => {
     Obs.observe(EL, obsOptions);
 });
+
+// Trigger toggleControlCenter function on window resize
+window.addEventListener('resize', function () {
+    toggleControlCenter();
+});
+
+// Function to toggle control center visibility
+function toggleControlCenter() {
+    var controlCenter = document.getElementById("control-center");
+    var menuBtn = document.getElementById("menu-btn");
+    var headerHeight = document.querySelector("header").offsetHeight;
+
+    if (controlCenter.style.top === headerHeight + "px") {
+        // Close the control center
+        controlCenter.style.top = "-100%";
+        controlCenter.style.height = "auto"; // Reset height to auto
+        menuBtn.style.display = "block"; // Show menu button
+    } else {
+        // Open the control center
+        controlCenter.style.top = headerHeight + "px"; // Position below header
+        controlCenter.style.height = "calc(50% - " + headerHeight + "px)"; // Adjust height as needed
+        menuBtn.style.display = "none"; // Hide menu button
+    }
+}
+
+function updateTimer() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const time = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('timer').textContent = time;
+}
+
+// Update timer every second
+setInterval(updateTimer, 1000);
+
+// Call updateTimer once immediately to display the initial time
+updateTimer();
+
+// Get the slider and value span elements
+const slider = document.getElementById('brightness-slider');
+const sliderValue = document.getElementById('brightness-value');
+
+// Add an event listener to update the displayed value when slider value changes
+slider.addEventListener('input', () => {
+    // Update the displayed value with the current slider value
+    const value = slider.value;
+    const gradient = `linear-gradient(to right, #007bff 0%, #007bff ${value}%, #ddd ${value}%, #ddd 100%)`;
+    slider.style.background = gradient;
+    const brightnessValue = slider.value;
+    document.documentElement.style.filter = `brightness(${brightnessValue}%)`;
+});
+
